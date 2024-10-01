badbf9f1abc57b6d9536f4e4d18591be7c707d9e
Jose Luis Ayuso Alonso <wikiestopa@gmail.com>
2024-10-01 16:18:00 +0200

    Add project files.

---
+++ Ab_Programacion.cpp
@@ -0,0 +1,12 @@
+﻿// Ab_Programacion.cpp : Defines the entry point for the application.
+//
+
+#include "Ab_Programacion.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
